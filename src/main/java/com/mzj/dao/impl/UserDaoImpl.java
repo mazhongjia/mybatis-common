62e8fe15ae21a3df7167d987eeb1a6e7b4c1a212
mazhongjia <mazhongjia126@126.com>
2020-01-07 17:44:25 +0800

    初版提交

---
+++ src/main/java/com/mzj/dao/impl/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package com.mzj.dao.impl;
+
+/**
+ * @Auther: mazhongjia
+ * @Date: 2020/1/7 16:03
+ * @Version: 1.0
+ */
+import com.mzj.dao.UserDao;
+import com.mzj.pojo.User;
+import org.apache.ibatis.session.SqlSession;
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    public SqlSession sqlSession;
+
+    public UserDaoImpl(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
+
+    @Override
+    public User queryUserById(String id) {
+        return this.sqlSession.selectOne("UserDao.queryUserById", id);
+    }
+
+    @Override
+    public List<User> queryUserAll() {
+        return this.sqlSession.selectList("UserDao.queryUserAll");
+    }
+
+    @Override
+    public void insertUser(User user) {
+        this.sqlSession.insert("UserDao.insertUser", user);
+    }
+
+    @Override
+    public void updateUser(User user) {
+        this.sqlSession.update("UserDao.updateUser", user);
+    }
+
+    @Override
+    public void deleteUser(String id) {
+        this.sqlSession.delete("UserDao.deleteUser", id);
+    }
+
+}
+
---
+++ src/main/java/com/mzj/interceptor/ExamplePlugin.java
@@ -0,0 +1,31 @@
+package com.mzj.interceptor;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.*;
+
+import java.util.Properties;
+
+/**
+ * @Auther: mazhongjia
+ * @Date: 2020/1/7 16:50
+ * @Version: 1.0
+ */
+// ExamplePlugin.java
+@Intercepts({@Signature(
+        type= Executor.class,
+        method = "update",
+        args = {MappedStatement.class,Object.class})})
+public class ExamplePlugin implements Interceptor {
+    public Object intercept(Invocation invocation) throws Throwable {
+        System.out.println("11111");
+        return invocation.proceed();
+    }
+    public Object plugin(Object target) {
+        System.out.println("22222");
+        return Plugin.wrap(target, this);
+    }
+    public void setProperties(Properties properties) {
+    }
+}
+
